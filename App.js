5a90b2fe7e1d0b52620d2519eb385cdae804cad9
dark7py <matveym20022002@gmail.com>
2024-01-21 11:01:19 +0500

    settings

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {WebView} from 'react-native-webview';
+import { Header } from "./components";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+    // children: [
+    //   {
+    //     path: "history",
+    //     element: <HistoryPage />,
+    //   },
+    //   {
+    //     path: "main",
+    //     element: <MainPage />,
+    //   },
+    //   {
+    //     path: "",
+    //     element: <MainPage />,
+    //   },
+    // ],
+  },
+]);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RouterProvider router={router}>
+      <View style={styles.container}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <WebView source={{ uri: "https://www.educative.io/" }} />
+        </SafeAreaView>
+        <StatusBar style="auto" />
+      </View>
+    </RouterProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/Header/Header.jsx
@@ -0,0 +1,100 @@
+import { StyleSheet, View } from "react-native";
+
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { message } from "antd";
+// import useStore from "@/store/store";
+import { HomeIcon, TimeIcon, IconWrapper } from "../../icons";
+// import { LoginModalComponent, RegistrationModalComponent } from "..";
+// import styles from "./styles.module.css";
+import { LogoutIcon } from "../../icons/LogoutIcon";
+import RegisterIcon from "../../icons/RegisterIcon";
+
+const tabs = [
+  {
+    path: "/main",
+    icon: <HomeIcon />,
+    title: "Главная",
+    id: "1",
+  },
+  {
+    path: "/history",
+    icon: <TimeIcon />,
+    title: "История",
+    id: "2",
+  },
+];
+
+export const Header = () => {
+  // const { modal, setModal, setNotificationMessage } = useStore();
+  // const [messageApi, contextHolder] = message.useMessage();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [selectedTab, setSelectedTab] = useState("/main");
+
+  useEffect(() => {
+    setSelectedTab(location.pathname);
+    // setNotificationMessage(messageApi);
+  }, [location]);
+
+  const handleOnTabClick = (tab) => {
+    return () => {
+      navigate(tab);
+    };
+  };
+  // function handleOnLoginClick(): void {
+  //   setModal(<LoginModalComponent />);
+  // }
+
+  // function handleOnRegisterClick(): void {
+  //   setModal(<RegistrationModalComponent />);
+  // }
+
+  function handleLogoutClick() {
+    localStorage.removeItem("token");
+    navigate("/main");
+  }
+
+  return (
+    <>
+      <View style={styles.header}>
+        <View style={styles.headerItems}>
+          {tabs.map((tab) => {
+            const isTabSelected = selectedTab === tab.path;
+
+            return (
+              <div
+                // className={`cursor-pointer font-bold ${
+                //   isTabSelected ? "" : ` ${styles.tabSelected}`
+                // }`}
+                key={tab.id}
+                onClick={handleOnTabClick(tab.path)}
+              >
+                <IconWrapper>
+                  {tab.icon} <div className="sm:hidden">{tab.title}</div>
+                </IconWrapper>
+              </div>
+            );
+          })}
+        </View>
+      </View>
+      <Outlet />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerItems: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+  },
+});
